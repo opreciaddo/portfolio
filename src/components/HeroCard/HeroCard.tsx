73f9e6f8c7fe170f3718cbbacf1e8d0980eff346
opreciaddo <o.preciaddo@gmail.com>
2024-02-21 19:01:42 -0600

    First components created

---
+++ src/components/HeroCard/HeroCard.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './HeroCard.css';
+
+interface HeroCardProps {}
+
+const HeroCard: FC<HeroCardProps> = () => (
+  <div className="HeroCard">
+    HeroCard Component
+  </div>
+);
+
+export default HeroCard;
